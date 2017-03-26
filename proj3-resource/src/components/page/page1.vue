64a87064fe7a061a536aceaf653d47e15bf0b4ea
Jam <guo3jin3cai@gmail.com>
2017-03-26 23:22:19 +0800

    local json

--- proj3-resource/src/components/page/page1.vue
+++ proj3-resource/src/components/page/page1.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="page">
     <h1>{{data}} {{reverseData}}</h1>
-    <div v-on:click="getIndexData">点击</div>
+    <!-- <div v-on:click="getdataNewGame">点击</div> -->
     <div>{{data}}</div>
     <ul class="newGame">
-		<li class="game-li">
+		<li class="game-li" v-for="game in dataNewGame">
+			<a class="game-one" href="#">
+				<img class="game-img" v-bind:src="game.icon_url"/>
+				<h4 class="game-name">{{game.app_name}}</h4>
+				<p class="game-des">{{game.category_name}}</p>
+				<p class="game-des">{{game.short_desc}}</p>
+			</a>
+			<a class="game-btn" v-bind:href="game.andriod_url">下载</a>
+		</li>
+		<!-- <li class="game-li">
 			<a class="game-one" href="#">
 				<img class="game-img" src="http://img01.starjoys.com/p_upload/2017/0103/1483411875926183.jpg"/>
 				<h4 class="game-name">游戏名称</h4>
@@ -12,7 +21,7 @@
 				<p class="game-des">简单描述描述</p>
 			</a>
 			<a class="game-btn" href="#">下载24</a>
-		</li>
+		</li> -->
 	</ul>
   </div>
 </template>
@@ -27,8 +36,11 @@
 		data(){
 			return {
 				data: 'page1',
-				indexData: {}
+				dataNewGame:{}	//应该放在app.vue?放在vuex的管理？不然每次都是重新请求
 			}
+		},
+		created(){
+			this.getdataNewGame();
 		},
 		computed:{
 			reverseData: function(){
@@ -36,17 +48,17 @@
 			}
 		},
 		methods: {
-			getIndexData: function(){
+			getdataNewGame: function(){
 				var instance = this;
 				// this.$http.get('./src/data/index.json').then((response) => {
 				instance.$http.get('./src/data/index.json').then((response) => {
 					    console.log(response.body.data.newGame);
-					    instance.indexData = response.body.data.newGame;
-					    // console.log(this.indexData);
+					    instance.dataNewGame = response.body.data.newGame;
+					    // console.log(this.dataNewGame);
 					    // return response.json();
-					    // this.$set(indexData, response.body);
-					    // instance.$set(instance.indexData,response.body.data);
-					    console.log(instance.indexData);
+					    // this.$set(dataNewGame, response.body);
+					    // instance.$set(instance.dataNewGame,response.body.data);
+					    console.log(instance.dataNewGame);
 					}, (response) => {
 					    // error callback
 					});
