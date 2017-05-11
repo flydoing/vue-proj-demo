d303606a5a0050a73cabc3d6ef080b96853fddaa
Jam <guo3jin3cai@gmail.com>
2017-05-11 22:24:48 +0800

    add detail.vue

---
+++ proj5-shop/src/components/cate/detail.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="s-cart">
+    <div class="cart-cont">
+      <ul>
+        <li class="cont-one" v-for="(cart, index) in carts">
+          <input class="goods-checkbox" type="checkbox" v-model="cart.isSelect" @click="clickSelect(cart)">
+          <a class="goods-a" href="javascript:;">
+            <img class="goods-img" :src="cart.cart_img">
+          </a>
+          <div class="goods-info">
+            <h5 class="goods-name">{{cart.cart_name}}</h5>
+            <div class="goods-counter">
+              <a href="javascript:;" class="btn-sub" @click="changeNum(-1, cart)"> - </a>
+              <input type="text" class="goods-num" readonly="readonly" v-model.lazy="cart.cart_num">
+              <a href="javascript:;" class="btn-add" @click="changeNum(1, cart)"> + </a>
+            </div>
+          </div>
+          <span class="goods-price">￥{{cart.cart_price*cart.cart_num}}</span>
+          <a class="goods-delete" href="javascript:;" @click="delectCart(index, carts)">删除</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="cart-counter">
+      <div class="all-checkbox">
+        <input type="checkbox" name="all-goods" class="all-goods" v-model="isAllSelectState" @click="clickAllSelect">
+        <label for="all-goods">全选</label>
+      </div>
+      <div class="all-price">
+        <p class="price-p">总计 :<em class="price">￥{{totalNowPrice}}</em></p>
+      </div>
+      <a class="btn-counter">去结算</a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import '../../css/cart.scss'
+
+  export default {
+    data () {
+      return {
+        dataCart: {},
+        carts: {},
+        isAllSelectState: false
+      }
+    },
+    created () {
+      this.$store.dispatch('changeHeaderTitle', '购物车')
+      this.getDataCart()
+    },
+    computed: {
+      totalNowPrice () {
+        let price = 0
+        console.log(typeof (this.carts))
+        // this.carts.forEach(cart => {    // 可以执行，但是报错this.carts.forEach is not a function
+        Array.from(this.carts).forEach(cart => {
+        // [...this.carts].forEach(cart => {   //SyntaxError: Unexpected token
+          if (cart.isSelect) {
+            price += cart.cart_price * cart.cart_num
+          }
+        })
+        console.log(price)
+        return price
+      }
+    },
+    methods: {
+      showSideBar () {
+        return this.$store.dispatch('changeSideBarState', true)
+        // return this.$store.commit('changeSideBarState', true)
+      },
+      hideSideBar () {
+        return this.$store.dispatch('changeSideBarState', false)
+      },
+      getDataCart () {
+        this.$http.get('../../static/data/cart.json').then((response) => {
+          this.dataCart = response.data
+          this.carts = this.dataCart.data.carts
+          // console.log(this.carts)
+        }, (response) => {
+          // error
+        })
+      },
+      changeNum (change, cart) {
+        if (change === -1) {
+          if (cart.cart_num >= 2) {
+            cart.cart_num = cart.cart_num - 1
+            console.log(cart.cart_num)
+          }
+        } else {
+          cart.cart_num = cart.cart_num + 1
+          console.log(cart.cart_num)
+        }
+      },
+      clickSelect (cart) {
+        cart.isSelect = !cart.isSelect  // 没有这个，数据没及时同步到全选
+        let isAllSelectState = Array.from(this.carts).every(cart => {
+          console.log(cart.isSelect)
+          return cart.isSelect
+        })
+        this.isAllSelectState = isAllSelectState
+        // console.log(this.isAllSelectState)
+      },
+      clickAllSelect () {
+        this.isAllSelectState = !this.isAllSelectState
+        Array.from(this.carts).forEach(cart => {
+        // [...this.carts].forEach(cart => {   //SyntaxError: Unexpected token
+          cart.isSelect = this.isAllSelectState
+        })
+      },
+      delectCart (index, carts) {
+        carts.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scope>
+
+</style>
