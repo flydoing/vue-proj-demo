54e991d51188875a7d04493b76faec77d99fc5d5
Jam <guo3jin3cai@gmail.com>
2017-05-15 23:46:07 +0800

    detail.vue

--- proj5-shop/src/components/cate/detail.vue
+++ proj5-shop/src/components/cate/detail.vue
@@ -1,53 +1,40 @@
 <template>
-  <div class="s-cart">
-    <div class="cart-cont">
-      <ul>
-        <li class="cont-one" v-for="(cart, index) in carts">
-          <input class="goods-checkbox" type="checkbox" v-model="cart.isSelect" @click="clickSelect(cart)">
-          <a class="goods-a" href="javascript:;">
-            <img class="goods-img" :src="cart.cart_img">
-          </a>
-          <div class="goods-info">
-            <h5 class="goods-name">{{cart.cart_name}}</h5>
-            <div class="goods-counter">
-              <a href="javascript:;" class="btn-sub" @click="changeNum(-1, cart)"> - </a>
-              <input type="text" class="goods-num" readonly="readonly" v-model.lazy="cart.cart_num">
-              <a href="javascript:;" class="btn-add" @click="changeNum(1, cart)"> + </a>
-            </div>
-          </div>
-          <span class="goods-price">￥{{cart.cart_price*cart.cart_num}}</span>
-          <a class="goods-delete" href="javascript:;" @click="delectCart(index, carts)">删除</a>
-        </li>
-      </ul>
+  <div class="s-detail">
+    <comSwiper></comSwiper>
+    <div class="cont">
+      <p class="name">{{detailData.name}}</p>
+      <span class="price">￥{{detailData.price}}</span>
+      <div class="goods-counter">
+        <a href="javascript:;" class="btn-sub" @click="changeNum(-1, detailData)"> - </a>
+        <input type="text" class="goods-num" readonly="readonly" v-model="detailData.num">
+        <a href="javascript:;" class="btn-add" @click="changeNum(1, detailData)"> + </a>
+      </div>
     </div>
-
-    <div class="cart-counter">
-      <div class="all-checkbox">
-        <input type="checkbox" name="all-goods" class="all-goods" v-model="isAllSelectState" @click="clickAllSelect">
-        <label for="all-goods">全选</label>
-      </div>
-      <div class="all-price">
-        <p class="price-p">总计 :<em class="price">￥{{totalNowPrice}}</em></p>
-      </div>
-      <a class="btn-counter">去结算</a>
+    <div class="bot">
+      <a href="javascript:;">加入购物车</a>
     </div>
   </div>
 </template>
 
 <script>
-
-  import '../../css/cart.scss'
+  import comSwiper from '../com/swiper'
+  import '../../css/detail.scss'
 
   export default {
     data () {
       return {
-        dataCart: {},
-        carts: {},
-        isAllSelectState: false
+        detailData: {
+          name: '商品名字55',
+          price: 23,
+          num: 1
+        }
       }
     },
+    components: {
+      comSwiper: comSwiper
+    },
     created () {
-      this.$store.dispatch('changeHeaderTitle', '购物车')
+      this.$store.dispatch('changeHeaderTitle', '商品详情页')
       this.getDataCart()
     },
     computed: {
@@ -82,35 +69,16 @@
           // error
         })
       },
-      changeNum (change, cart) {
+      changeNum (change, detail) {
         if (change === -1) {
-          if (cart.cart_num >= 2) {
-            cart.cart_num = cart.cart_num - 1
-            console.log(cart.cart_num)
+          if (detail.num >= 2) {
+            detail.num = detail.num - 1
+            console.log(detail.num)
           }
         } else {
-          cart.cart_num = cart.cart_num + 1
-          console.log(cart.cart_num)
+          detail.num = detail.num + 1
+          console.log(detail.num)
         }
-      },
-      clickSelect (cart) {
-        cart.isSelect = !cart.isSelect  // 没有这个，数据没及时同步到全选
-        let isAllSelectState = Array.from(this.carts).every(cart => {
-          console.log(cart.isSelect)
-          return cart.isSelect
-        })
-        this.isAllSelectState = isAllSelectState
-        // console.log(this.isAllSelectState)
-      },
-      clickAllSelect () {
-        this.isAllSelectState = !this.isAllSelectState
-        Array.from(this.carts).forEach(cart => {
-        // [...this.carts].forEach(cart => {   //SyntaxError: Unexpected token
-          cart.isSelect = this.isAllSelectState
-        })
-      },
-      delectCart (index, carts) {
-        carts.splice(index, 1)
       }
     }
   }
--- proj5-shop/src/router/router.js
+++ proj5-shop/src/router/router.js
@@ -5,6 +5,7 @@ Vue.use(VueRouter)
 import App from '../App.vue'
 import Index from '../components/page/index.vue'
 import Cate from '../components/cate/cate.vue'
+import Detail from '../components/cate/detail.vue'
 import Center from '../components/center/center.vue'
 import Cart from '../components/cart/cart.vue'
 
@@ -17,6 +18,7 @@ export default new VueRouter({
       children: [
         {path: 'index', name: 'index', component: Index},
         {path: 'cate', name: 'cate', component: Cate},
+        {path: 'detail', name: 'detail', component: Detail},
         {path: 'center', name: 'center', component: Center},
         {path: 'cart', name: 'cart', component: Cart}
       ]
