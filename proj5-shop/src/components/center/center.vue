f2808d963ce5325e9bcc93d3ed7b668ef720551d
Jam <guo3jin3cai@gmail.com>
2017-06-03 10:52:28 +0800

    center done

--- proj5-shop/src/components/center/center.vue
+++ proj5-shop/src/components/center/center.vue
@@ -15,22 +15,28 @@
         <a href="javascript:;">我的优惠券<i>></i></a>
         <a href="javascript:;">个人设置<i>></i></a>
       </div>
+      <div class="btn-div"><a href="javascript:;" class="btn-a" @click="clickLogout">退出登录</a></div>
     </div>
 
     <div class="cont-register" v-else-if="showState === 'register'">
       <div class="form-item">
         <label for="mobile">手机号</label>
-        <input name="mobile" type="tel" placeholder="请输入手机号" maxlength="11" class="">
+        <input name="mobile" type="tel" placeholder="请输入手机号" maxlength="11" v-model="dataLogin.name">
       </div>
       <div class="form-item">
         <label for="code">验证码</label>
-        <input class="" name="code" type="tel" placeholder="请输入验证码" maxlength="6">
-        <button class="btn-get" type="button">重新获取</button>
+        <input class="" name="code" type="tel" placeholder="请输入验证码" maxlength="6" v-model="dataLogin.code">
+        <a href="javascript:;" class="btn-get">重新获取</a>
       </div>
-      <div class="btn-div"><a href="javascript:;" class="btn-a">注册按钮</a></div>
+      <div class="form-item">
+        <label for="password">密&nbsp;&nbsp;&nbsp;码</label>
+        <input name="password" type="tel" placeholder="请输入密码" maxlength="6" v-model="dataLogin.pass">
+        <a class="btn-get" href="javascript:;">眼睛</a>
+      </div>
+      <div class="btn-div"><a href="javascript:;" class="btn-a" @click="clickRegister">注册按钮</a></div>
       <fieldset class="">
         <legend>或</legend>
-        <a href="javascript:;" class="">已有账号？快速登录</a>
+        <a href="javascript:;" class="" @click="toLogin">已有账号？快速登录</a>
       </fieldset>
     </div>
 
@@ -47,7 +53,7 @@
       <div class="btn-div"><a href="javascript:;" class="btn-a" @click="clickLogin">登录按钮</a></div>
       <fieldset class="form-fieldset">
         <legend>或</legend>
-        <a href="javascript:;" class="">未有账号？免费注册</a>
+        <a href="javascript:;" class="" @click="toRegister">未有账号？免费注册</a>
       </fieldset>
     </div>
   </div>
@@ -71,11 +77,12 @@
         showState: 'logined',
         dataLogin: {
           name: '',
-          pass: ''
+          pass: '',
+          code: ''
         },
-        jam: function () {}
         // showState: 'register'
         // showState: 'logining'
+        jam: function () {}
       }
     },
     created () {
@@ -84,7 +91,8 @@
       // this.jam.locDbSet('dataLogin', {name: 'guojc1', pass: 7896})
       let lDataLogin = this.jam.locDbGet('dataLogin')
       if (lDataLogin === undefined || lDataLogin === null) {
-        this.showState = 'logining'
+        // this.showState = 'logining'
+        this.showState = 'register'
       } else {
         this.dataLogin = this.jam.locDbGet('dataLogin')
         this.showState = 'logined'
@@ -110,12 +118,41 @@
             // 登录成功
             this.jam.locDbSet('dataLogin', {name: this.dataLogin.name, pass: this.dataLogin.pass})
             this.showState = 'logined'
+            this.tips = ''
           } else {
             this.tips = '请输入由字母数字组成的6位密码！'
           }
         } else {
           this.tips = '请输入正确的手机号！'
         }
+      },
+      clickRegister () {
+        // 验证
+        console.log(this.dataLogin)
+        if (this.jam.isPhone(this.dataLogin.name)) {
+          if (this.jam.isPass(this.dataLogin.pass) && this.jam.isPass(this.dataLogin.code)) {
+            // 登录成功
+            this.jam.locDbSet('dataLogin', {name: this.dataLogin.name, pass: this.dataLogin.pass})
+            this.showState = 'logined'
+            this.tips = ''
+          } else {
+            this.tips = '请输入由字母数字组成的6位验证码密码！'
+          }
+        } else {
+          this.tips = '请输入正确的手机号！'
+        }
+      },
+      clickLogout () {
+        localStorage.removeItem('dataLogin')
+        this.showState = 'logining'
+        this.dataLogin.name = ''
+        this.dataLogin.pass = ''
+      },
+      toRegister () {
+        this.showState = 'register'
+      },
+      toLogin () {
+        this.showState = 'logining'
       }
     }
   }
